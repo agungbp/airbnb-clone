76360b971820747a663bd97f651b6253f7d804db
Agung Budie Pangestu <agungbudiepangestu@gmail.com>
2024-05-28 11:00:08 +0700

    create favorites & properties page

---
+++ app/favorites/page.tsx
@@ -0,0 +1,36 @@
+import EmptyState from "../components/EmptyState"
+import getCurrentUser from "../actions/getCurrentUser"
+import getFavoriteListings from "../actions/getFavoriteListings"
+import FavoritesClient from "./FavoritesClient"
+
+const FavoritesPage = async () => {
+    const listings = await getFavoriteListings()
+    const currentUser = await getCurrentUser()
+
+    if (listings.length === 0) {
+        return (
+            <EmptyState
+                title="No favorites found"
+                subtitle="Looks like you have no favorite listings."
+            />
+        )
+    }
+
+    if (!currentUser) {
+        return (
+            <EmptyState
+                title="Unauthorized"
+                subtitle="Please login"
+            />
+        )
+    }
+
+    return (
+        <FavoritesClient
+            listings={listings}
+            currentUser={currentUser}
+        />
+    );
+}
+
+export default FavoritesPage;
